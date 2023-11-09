80af9245324d2bae754a5d7e5d0bbe53b1b833df
armoney7 <112188406+armoney7@users.noreply.github.com>
2023-11-09 22:42:17 +0700

    add filterDoctor

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,12 +2,14 @@ import 'normalize.css'
 import './App.scss'
 
 import Articles from '../articles/Articles'
+import FilterDoctor from '../filterDoctor/FilterDoctor'
 
 function App() {
 
 	return (
 		<>
 			<Articles />
+			<FilterDoctor/>
 		</>
 	)
 }
